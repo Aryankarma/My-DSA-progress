c6374ebb02d8eb78c328f472426ee013ccbf9828
aryankarma <aryankarma29@gmail.com>
2023-07-28 21:59:01 +0530

    updating bubble sort

--- Lecture16181719/BubbleSort.cpp
+++ Lecture16181719/BubbleSort.cpp
@@ -8,15 +8,32 @@ int main(){
     int size = sizeof(array)/sizeof(array[0]);
     cout << size << endl;
 
-    for(int i=1; i<size; i++){
+    // for(int i=1; i<size; i++){
+    //     for(int j=0; j<size-1; j++){
+    //         if(array[j] > array[j+1]){
+    //             swap(array[j], array[j+1]);
+    //         }
+    //     }
+    // }
+
+    // for(int i=0; i<size; i++){
+    //     cout << array[i] << " ";
+    // }
+    for(int i=1; i<size;i++){
+    bool swapped = false;
         for(int j=0; j<size-1; j++){
-            if(array[j] > array[j+1]){
-                swap(array[j], array[j+1]);
+            if(array[j]>array[j+1]){
+                swap(array[j],array[j+1]);
+                swapped = true;
             }
         }
+        if(swapped == false){
+            break;
+            // the array is already sorted;
+        }
     }
-
     for(int i=0; i<size; i++){
         cout << array[i] << " ";
     }
+
 }
