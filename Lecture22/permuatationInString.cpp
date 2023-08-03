446971e618d70925306475497a32ca906d48a97e
aryankarma <aryankarma29@gmail.com>
2023-08-03 21:10:44 +0530

    update

--- Lecture22/permuatationInString.cpp
+++ Lecture22/permuatationInString.cpp
@@ -1,6 +1,82 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+/*  problem statement - 567. Permutation in String
+    Given two strings s1 and s2, return true if s2 contains a permutation of s1, 
+    or false otherwise.
+
+    In other words, return true if one of s1's permutations is the substring of s2.
+
+    Example 1:
+    Input: s1 = "ab", s2 = "eidbaooo"
+    Output: true
+    Explanation: s2 contains one permutation of s1 ("ba")
+
+    Example 2:
+    Input: s1 = "ab", s2 = "eidboaoo"
+    Output: false
+*/
+
+// Link to LeetCode [https://leetcode.com/problems/permutation-in-string/]
+
+// Leetcode solution
+
+
+class Solution {
+bool checkEqual(int arr1[26], int arr2[26]){
+    for(int i=0; i<26; i++){
+        if(arr1[i] != arr2[i]){
+            return 0;
+        }
+    }
+    return 1;
+}
+
+public:
+  bool checkInclusion(string s1, string s2) {
+    int arr1[26] = {0};
+
+    for(int i=0; i<s1.length(); i++){
+        int index = s1[i] - 'a';
+        arr1[index]++;
+    }
+
+    int arr2[26] = {0};
+    int windowSize = s1.length(); 
+    int i=0; 
+
+    while(i<windowSize && i<s2.length()){
+        int index = s2[i] - 'a';
+        arr2[index]++;
+        i++;
+    }
+
+    if(checkEqual(arr1, arr2)){
+        return 1;
+    }
+    
+    while(i < s2.length()){
+        int index = s2[i] - 'a';
+        arr2[index]++;
+
+        int old = s2[i- windowSize];
+        index = old - 'a';
+        arr2[index]--;   
+        i++;
+        
+        if(checkEqual(arr1, arr2)){
+        return 1;
+    }
+    }
+    return 0;
+  }
+};
+
+
+
+
+
+// VS code solution
 bool arrayIsEqual(int arr1[26], int arr2[26]){
     for(int i=0; i<26; i++){
         if(arr1[i] != arr2[i]){
