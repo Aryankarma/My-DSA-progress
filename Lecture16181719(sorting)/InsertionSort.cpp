713d68d9438190dbecee81a3f255d64d9b37d690
aryankarma <aryankarma29@gmail.com>
2023-09-04 21:52:39 +0530

    renamed

---
+++ Lecture16181719(sorting)/InsertionSort.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int array[10] = {4,564,5,5,12,32,564,54,0,12};
+    int size = sizeof(array)/sizeof(array[0]);
+    for(int i=0; i<size; i++){
+        int temp = array[i];
+        int j= i-1;
+        for( ; j>=0; j--){
+            
+             if(array[j] > temp) {
+                // aligning the order
+                array[j+1] = array[j];
+            }       
+            else { 
+                break;
+            }
+        }
+    array[j+1] = temp;
+    }
+
+
+    for(int i=0; i<size; i++){
+        cout << array[i] << " ";
+    }
+
+}
+
+// This technique is adaptable and stable -
+
+/*  Adaptable means that an algorithm that works on the go, which can output half of the 
+    progress if we stop somewhere in between the process.  
+    Stable means that it doesn't process unnecessary things like swapping same elements 
+    in the array, this instability can be found in quick sort, heap sort and selection sort.
+*/
---
+++ Lecture16181719(sorting)/SelectionSort.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// Simple selection sort Program
+
+/*  in selection sort we use two pointers, the first one is set to the zeroth element of the array
+    and the second one is set to one element ahead of the first element. the second element 
+    tries to find the smallest number in the array while the first pointer is fixed at the 
+    first element of the array, when it finally finds the smallest number in the array except 
+    first element the loop breaks and it gets swapped with the first pointer, in the next step the first 
+    pointer get's updated and the process goes on
+*/
+
+int main(){ 
+  int array[15] = {564,5,1542,564,987,987,12,48,84,9,56,1,12,5,48};
+  int size = sizeof(array)/sizeof(array[0]);
+
+  // for(int i=0; i<size; i++){
+  //   int minIndex = i;
+  //   for(int j=i+1; j<size; j++){
+  //       if(array[j] < array[minIndex])
+  //           swap(array[j], array[minIndex]);
+  //   }
+  // }
+  //   for(int i=0; i<size; i++){
+  //       cout << array[i] << " ";
+  //   }
+
+  for(int i=0; i<size; i++){
+    int minIndex = i;
+    for(int j=i+1; j<size; j++){
+        if(array[j] < array[minIndex])
+        minIndex = j;
+    }
+    swap(array[i], array[minIndex]);
+  }
+    for(int i=0; i<size; i++){
+        cout << array[i] << " ";
+    }
+}
