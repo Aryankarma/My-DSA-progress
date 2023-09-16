690236bd5b9958313898d24c409f3a37ad55cccf
aryankarma <aryankarma29@gmail.com>
2023-09-16 23:59:33 +0530

    part 2

--- Lecture38/PhoneKeypadProblem+letterCombinartionOfphoneNumber.cpp
+++ Lecture38/PhoneKeypadProblem+letterCombinartionOfphoneNumber.cpp
@@ -35,6 +35,7 @@ using namespace std;
     digits[i] is a digit in the range ['2', '9']
 
 */
+
 class Solution{
 private:
     void solve(string digits, string inner, vector<string> &outer, string map[], int index){
@@ -65,3 +66,49 @@ public:
         return outer;
     }
 };
+
+
+// vs code solution:
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(string digits, string inner, vector<string> &outer, string map[], int index){
+    // base case
+    if (index >= digits.length()){
+        outer.push_back(inner);
+        return;
+    }
+    cout << "index : " << index << endl;
+    int number = digits[index] - '0';
+    cout << "number : " << number << endl;
+    string value = map[number];
+    cout << "value :  " << value << endl;
+    for (int i = 0; i < value.length(); i++){
+        inner.push_back(value[i]);
+        cout << "inner :  " << inner << endl;
+        solve(digits, inner, outer, map, index + 1);
+        cout << "calling after recursive & pop back :  " << endl;
+        cout << inner.back() << endl;
+        inner.pop_back();
+    }
+}
+
+vector<string> letterCombinations(string digits){
+    string inner = "";
+    vector<string> outer;
+    if (digits.length() == 0){
+        return outer;
+    }
+    int index = 0;
+    string mappletter[10] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    solve(digits, inner, outer, mappletter, index);
+    return outer;
+}
+
+int main(){
+    vector<string> save = letterCombinations("23");
+    for(int i=0; i<save.size(); i++){
+        cout << save[i] << " "; 
+    }
+}
